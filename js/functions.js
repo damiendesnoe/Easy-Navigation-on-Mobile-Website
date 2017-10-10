1fb400d537da4d4cab1927805224355616f9c560
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-10 18:37:18 +0200

    Added the "reset" style and body fixed position

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,9 @@
 (function () {
 	document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
-	
+
 	function showHideMenu() {
-		var navE = document.getElementById("mobile-nav");
+		document.body.classList.toggle("menu-active");
 		document.getElementById("nav-toggle").classList.toggle("active");
-		
-		navE.classList.toggle("active");
-			
-		if (!navE.classList.contains("active")) {
-			var aEl = document.getElementsByTagName("a");
-			var liE = document.getElementsByTagName("li");
-			for (var i = 0; i < aEl.length; i++) {
-				aEl[i].style.display = "block";
-				liE[i].style.display = "block";
-			}
-			setTimeout(function () {
-				for (var i = 0; i < aEl.length; i++) {
-					aEl[i].removeAttribute("style");
-					liE[i].removeAttribute("style");
-				}
-			}, 500);
-		}
+		document.getElementById("mobile-nav").classList.toggle("active");
 	}
 })();
