7cbfd3bc0fc2fbb3908687671efd9edb63e1a2d0
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-10 15:44:39 +0200

    Disable scrolling when menu is active Fix the menu button height

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,8 @@
 	document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
 
 	function showHideMenu() {
+		document.getElementsByTagName("html")[0].classList.toggle("menu-active");
 		document.getElementById("mobile-nav").classList.toggle("active");
-
 		document.getElementById("nav-toggle").classList.toggle("active");
 	}
 })();
