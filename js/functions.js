5fa4b34aae8c0f5c0ea99fdf552bea78688b6ffe
Damien Desnoë <damien.desnoe@gmail.com>
2017-10-10 15:20:45 +0200

    Fix bugs

--- js/functions.js
+++ js/functions.js
@@ -2,25 +2,8 @@
 	document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
 
 	function showHideMenu() {
-		var navE = document.getElementById("mobile-nav");
+		document.getElementById("mobile-nav").classList.toggle("active");
 
 		document.getElementById("nav-toggle").classList.toggle("active");
-		navE.classList.toggle("active");
-
-		// if (!navE.classList.contains("active")) {
-		// 	var aEl = document.getElementsByTagName("a");
-		// 	var liE = document.getElementsByTagName("li");
-		// 	for (var i = 0; i < aEl.length; i++) {
-		// 		aEl[i].style.display = "block";
-		// 		aEl[i].style.textTransform = "uppercase";
-		// 		liE[i].style.display = "block";
-		// 	}
-		// 	setTimeout(function () {
-		// 		for (var i = 0; i < aEl.length; i++) {
-		// 			aEl[i].removeAttribute("style");
-		// 			liE[i].removeAttribute("style");
-		// 		}
-		// 	}, 500);
-		// }
 	}
 })();
