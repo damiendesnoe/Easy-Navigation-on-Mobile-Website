a4b86d48b1503819f6d9b63e7e18f3cf2f0f9da0
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-11 16:34:15 +0200

    Make the 2D push left animation for desktop

--- js/functions.js
+++ js/functions.js
@@ -2,18 +2,22 @@
 	document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
 
 	function showHideMenu() {
-		document.body.classList.toggle("menu-active");
 		document.getElementById("mobile-nav").classList.toggle("active");
 		document.getElementById("nav-toggle").classList.toggle("active");
 
-		if(!document.getElementById("mobile-nav").classList.contains("active")) {
+		if(!document.body.classList.contains("menu-active")) {
 			document.body.classList.toggle("menu-active");
+		} else {
 			if (window.scrollY > 60) {
 				document.getElementsByTagName("header")[0].style.top = "-100%";
 			}
+
+			document.getElementById("wrapper").style.margin = "20px 200px";
+
 			setTimeout(function () {
 				document.body.classList.toggle("menu-active");
 				document.getElementsByTagName("header")[0].removeAttribute("style");
+				document.getElementById("wrapper").removeAttribute("style");
 			}, 500);
 		}
 	}
