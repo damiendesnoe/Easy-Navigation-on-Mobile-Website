473196ef6006d1276ff7c4cc183b9dddb182b067
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-11 14:55:21 +0200

    Remove commented CSS and adjust some positions

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@
 
 		if(!document.body.classList.contains("menu-active")) {
 			document.getElementsByTagName("header")[0].style.position = "fixed";
-			document.body.style.paddingTop = "57px";
+			document.body.style.paddingTop = "58px";
 			setTimeout(function () {
 				document.getElementsByTagName("header")[0].removeAttribute("style");
 				document.body.removeAttribute("style");
