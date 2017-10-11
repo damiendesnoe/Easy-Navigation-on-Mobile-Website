180b2924e9cf780290a83c152924a616e0ba5236
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-11 15:35:44 +0200

    Make fade in animation on desktop

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,23 @@
 	document.getElementById("nav-toggle").addEventListener("click", showHideMenu);
 
 	function showHideMenu() {
-		document.body.classList.toggle("menu-active");
 		document.getElementById("mobile-nav").classList.toggle("active");
 		document.getElementById("nav-toggle").classList.toggle("active");
 
 		if(!document.body.classList.contains("menu-active")) {
-			document.getElementsByTagName("header")[0].style.position = "fixed";
-			document.body.style.paddingTop = "58px";
+			document.body.classList.toggle("menu-active");
+		} else {
+			document.getElementById("mobile-nav").style.zIndex = "1";
+
+			if (window.scrollY > 60) {
+				document.getElementsByTagName("header")[0].style.opacity = "0";
+			}
+
 			setTimeout(function () {
+				document.body.classList.toggle("menu-active");
+				document.getElementById("mobile-nav").removeAttribute("style");
 				document.getElementsByTagName("header")[0].removeAttribute("style");
-				document.body.removeAttribute("style");
-			}, 100);
+			}, 700);
 		}
 	}
 })();
