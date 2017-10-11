a573ed57d3322abea1ee2d956e2cbcff5377d6ae
Damien Desnoë <damien.desnoe@gmail.com>
2017-10-11 17:20:14 +0200

    Make the animation for phone
    Disable the desktop js animation script on phone

--- js/functions.js
+++ js/functions.js
@@ -5,20 +5,24 @@
 		document.getElementById("mobile-nav").classList.toggle("active");
 		document.getElementById("nav-toggle").classList.toggle("active");
 
-		if(!document.body.classList.contains("menu-active")) {
+		if (window.innerWidth < 1024) {
 			document.body.classList.toggle("menu-active");
 		} else {
-			if (window.scrollY > 60) {
-				document.getElementsByTagName("header")[0].style.top = "-100%";
-			}
-
-			document.getElementById("wrapper").style.margin = "20px 200px";
-
-			setTimeout(function () {
+			if(!document.body.classList.contains("menu-active")) {
 				document.body.classList.toggle("menu-active");
-				document.getElementsByTagName("header")[0].removeAttribute("style");
-				document.getElementById("wrapper").removeAttribute("style");
-			}, 500);
+			} else {
+				if (window.scrollY > 60) {
+					document.getElementsByTagName("header")[0].style.top = "-100%";
+				}
+	
+				document.getElementById("wrapper").style.margin = "20px 200px";
+	
+				setTimeout(function () {
+					document.body.classList.toggle("menu-active");
+					document.getElementsByTagName("header")[0].removeAttribute("style");
+					document.getElementById("wrapper").removeAttribute("style");
+				}, 500);
+			}
 		}
 	}
 })();
