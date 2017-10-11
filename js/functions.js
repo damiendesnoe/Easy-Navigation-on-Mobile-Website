270e4a678fd34f5b8ca868cc9164241aae9d7ef0
Damien Desnoe <damien.desnoe@gmail.com>
2017-10-11 13:16:30 +0200

    Add slide up animation for header on desktop

--- js/functions.js
+++ js/functions.js
@@ -5,5 +5,16 @@
 		document.body.classList.toggle("menu-active");
 		document.getElementById("mobile-nav").classList.toggle("active");
 		document.getElementById("nav-toggle").classList.toggle("active");
+
+		if(!document.getElementById("mobile-nav").classList.contains("active")) {
+			document.body.classList.toggle("menu-active");
+			if (window.scrollY > 60) {
+				document.getElementsByTagName("header")[0].style.top = "-100%";
+			}
+			setTimeout(function () {
+				document.body.classList.toggle("menu-active");
+				document.getElementsByTagName("header")[0].removeAttribute("style");
+			}, 500);
+		}
 	}
 })();
